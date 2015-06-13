31b6381ae72d9fe6da4c7ba7302a40ee0c0fb45b
Erik Ebsen <erik.ebsen@me.com>
2015-06-12 22:12:18 -0500

    Display toggles working! Plus lots of other cleanup.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,70 @@
-// (function() {
-  var header = document.querySelector('header')
-  var headroom = new Headroom(header, {
-    'offset': 205,
-    'tolerance': 5,
-    'classes': {
-      'initial': 'animated',
-      'pinned': 'slideDown',
-      'unpinned': 'slideUp'
-    }
+(function() {
+
+
+
+  var projects           = document.querySelectorAll('.project')
+  var professionalToggle = document.getElementById('show-professional')
+  var personalToggle     = document.getElementById('show-personal')
+  var allToggle          = document.getElementById('show-all')
+  var projectToggles = [professionalToggle, personalToggle]
+  // console.log(projects)
+
+  // professionalToggle.onclick = showProfessional
+  personalToggle.onclick = showPersonal
+
+  projectToggles.forEach(function (toggle) {
+    toggle.onclick = toggleDisplay
   })
-  headroom.init()
-// })();
+  allToggle.onclick = displayAll
+
+  function displayAll() {
+    var toggle = this.id.replace('show-', '')
+    var classes
+    for (var i = 0; i < projects.length; i++) {
+      classes = projects[i].classList
+      if (classes.contains('hidden')) {
+        classes.remove('hidden')
+      }
+    }
+  }
+
+  function toggleDisplay() {
+    var toggle = this.id.replace('show-', '')
+    var classes
+    console.log(toggle)
+
+    for (var i = 0; i < projects.length; i++) {
+      classes = projects[i].classList
+      // Hide everything.
+      if (!classes.contains('hidden')) {
+        classes.add('hidden')
+      }
+      // Display the ones selected.
+      if (classes.contains(toggle)) {
+        classes.remove('hidden')
+      }
+    }
+  }
+
+  function showPersonal() {
+
+    // projects.forEach(function (project) {
+    //   var classes = project.classList
+    //   console.log(classes)
+    // })
+
+    for (var i = 0; i < projects.length; i++) {
+      // console.log(projects[i].classList)
+      var classes = projects[i].classList
+      if (classes.contains('personal')) {
+        classes.add('hidden')
+      }
+    }
+
+    // console.log(this)
+    // var classes = this.classList
+    // classes.length ? classes.toggles('hidden') : classes.add('hidden')
+    // this.classlist.toggle('hidden')
+  }
+
+})()
