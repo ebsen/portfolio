4d8e9dc6095bbb8c63e8285e0689b577a46594a0
Erik Ebsen <erik.ebsen@me.com>
2015-06-14 20:58:38 -0500

    Working animations between project types and less image size

---
+++ public/_pages/notes.md
@@ -0,0 +1,12 @@
+# CommonSpot
+
+> Thoughts: Emphasize leadership.
+> - overseeing development of new calendar system
+> - developing new features for Tuition and Fees information
+> - launching new School of Design site
+> - developing new ADRDL user guide
+> - coordinating development of new features across a team of three programmers
+>   - distinguished engineers
+>   - ADRDL
+>   - Tuition and Fees
+> - guiding the overhaul of calendar and event system by student programmer
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,70 +1,158 @@
 (function() {
 
+  function displayAll() {
+    var toggle = this.id.replace('show-', '');
+    var classes;
+    for (var i = 0; i < projects.length; i++) {
+      classes = projects[i].classList;
+      if (classes.contains('fadeOut')) {
+        classes.remove('fadeOut');
+      }
+      if (classes.contains('hidden')) {
+        classes.remove('hidden');
+      }
+      // IDEA: Remove event listeners here?
+    }
+  }
+
+  function hide() {
+    // console.log(this);
+    if (this.classList.contains('fadeOut')) {
+      this.classList.add('hidden');
+    }
+  }
+  
+  function toggleDisplay() {
+    // Determine which type of project to hide.
+    var toggle = this.id.replace('show-', '');
+    var classes;
+    var events = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    var type = 'webkitAnimationEnd'
+    // console.log(toggle);
+
+    for (var i = 0; i < projects.length; i++) {
+      classes = projects[i].classList;
+
+      // Hide everything
+      // if (!classes.contains('hidden')) {
+      //   classes.add('hidden')
+      // }
+      // Display the ones selected
+      // if (classes.contains(toggle)) {
+      //   classes.remove('hidden')
+      // }
+
+      // Reset our list.
+      if (classes.contains('fadeOut')) {
+        classes.remove('fadeOut');
+      }
+      if (classes.contains('hidden')) {
+        classes.remove('hidden');
+        classes.add('fadeIn')
+      }
+
+      // Apply the animation to remove it
+      if (!classes.contains(toggle)) {
+        classes.add('fadeOut');
+        // projects[i].addEventListener(type, hide);
+        // projects[i].removeEventListener(type, hide);
+      }
+      events.split(' ').forEach(function (type) {
+        projects[i].addEventListener(type, hide);
+      })
+    }
+  }
+
+  // var allToggle          = document.getElementById('show-all');
+  // var professionalToggle = document.getElementById('show-professional');
+  var projects           = document.querySelectorAll('.project');
+  // var projectToggles     = [professionalToggle, personalToggle];
+  // var personalToggle     = document.getElementById('show-personal');
+  var toggles = {
+    all:          document.getElementById('show-all'),
+    professional: document.getElementById('show-professional'),
+    personal:   document.getElementById('show-personal')
+  };
+
+  toggles.all.onclick = displayAll;
+  toggles.professional.onclick = toggleDisplay;
+  toggles.personal.onclick = toggleDisplay;
+
+  // for (var toggle in toggles) {
+  //   // console.log(toggle);
+  //   if (toggle !== 'all') {
+  //     // console.log(toggle);
+  //     toggle.onclick = toggleDisplay;
+  //   }
+  // }
 
 
-  var projects           = document.querySelectorAll('.project')
-  var professionalToggle = document.getElementById('show-professional')
-  var personalToggle     = document.getElementById('show-personal')
-  var allToggle          = document.getElementById('show-all')
-  var projectToggles = [professionalToggle, personalToggle]
+
+
   // console.log(projects)
 
   // professionalToggle.onclick = showProfessional
-  personalToggle.onclick = showPersonal
+  // personalToggle.onclick = showPersonal;
 
-  projectToggles.forEach(function (toggle) {
-    toggle.onclick = toggleDisplay
-  })
-  allToggle.onclick = displayAll
+  // projectToggles.forEach(function (toggle) {
+  //   toggle.onclick = toggleDisplay;
+  // });
+  // allToggle.onclick = displayAll;
 
-  function displayAll() {
-    var toggle = this.id.replace('show-', '')
-    var classes
-    for (var i = 0; i < projects.length; i++) {
-      classes = projects[i].classList
-      if (classes.contains('hidden')) {
-        classes.remove('hidden')
-      }
-    }
-  }
+  // projectToggles.forEach(function (toggle) {
+  //   toggle.addEventListener('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', hide);
+  // });
 
-  function toggleDisplay() {
-    var toggle = this.id.replace('show-', '')
-    var classes
-    console.log(toggle)
 
-    for (var i = 0; i < projects.length; i++) {
-      classes = projects[i].classList
-      // Hide everything.
-      if (!classes.contains('hidden')) {
-        classes.add('hidden')
-      }
-      // Display the ones selected.
-      if (classes.contains(toggle)) {
-        classes.remove('hidden')
-      }
-    }
-  }
+  // function displayAll() {
+  //   var toggle = this.id.replace('show-', '')
+  //   var classes
+  //   for (var i = 0; i < projects.length; i++) {
+  //     classes = projects[i].classList
+  //     if (classes.contains('hidden')) {
+  //       classes.remove('hidden')
+  //     }
+  //   }
+  // }
 
-  function showPersonal() {
+  // function toggleDisplay() {
+  //   var toggle = this.id.replace('show-', '')
+  //   var classes
+  //   console.log(toggle)
+  //
+  //   for (var i = 0; i < projects.length; i++) {
+  //     classes = projects[i].classList
+  //     // Hide everything
+  //     if (!classes.contains('hidden')) {
+  //       classes.add('hidden')
+  //     }
+  //     // Display the ones selected
+  //     if (classes.contains(toggle)) {
+  //       classes.remove('hidden')
+  //     }
+  //   }
+  // }
+
+
+  // function showPersonal() {
 
     // projects.forEach(function (project) {
     //   var classes = project.classList
     //   console.log(classes)
     // })
 
-    for (var i = 0; i < projects.length; i++) {
+    // for (var i = 0; i < projects.length; i++) {
       // console.log(projects[i].classList)
-      var classes = projects[i].classList
-      if (classes.contains('personal')) {
-        classes.add('hidden')
-      }
-    }
+      // var classes = projects[i].classList
+      // if (classes.contains('personal')) {
+        // classes.add('hidden')
+      // }
+    // }
 
     // console.log(this)
     // var classes = this.classList
     // classes.length ? classes.toggles('hidden') : classes.add('hidden')
     // this.classlist.toggle('hidden')
-  }
+  // }
 
-})()
+})();
